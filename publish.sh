64d9bc960840c72f81fb057e55c8fc856bcdaf3f
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-09-26 13:08:05 +0200

    Update publish dir

--- publish.sh
+++ publish.sh
@@ -7,22 +7,23 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf public
-mkdir public
+rm -rf docs
+mkdir docs
 git worktree prune
-rm -rf .git/worktrees/public/
+rm -rf .git/worktrees/docs/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out gh-pages branch into docs"
+git worktree add -B gh-pages docs origin/gh-pages
 
 echo "Removing existing files"
-rm -rf public/*
+rm -rf docs/*
+echo "foxgirls.gay" > docs/CNAME
 
 echo "Generating site"
 hugo -t github-style
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publish"
+cd docs && git add --all && git commit -m "Publish"
 
 #echo "Pushing to github"
 git push --all
